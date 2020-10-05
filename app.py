536bc89ef4499baa337f6fc8d144413bc73f7c03
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2020-10-05 20:52:04 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt      
+import matplotlib.pyplot as plt
+import plotly
 import plotly.express as px          
 import plotly.graph_objects as go    
 import plotly.figure_factory as ff
