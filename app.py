8dfff4fa84470ba51b22b801bba24d7a0f469b9a
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2020-11-15 21:18:18 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -656,8 +656,8 @@ def exp_geography():
         text = time_df['Eponym_easy'] + ', ' + time_df['CityOfEponym_A1'] + ', ' + time_df['Year'].astype(str)
         locations_name = time_df['Eponym_easy'] #df3['Eponym_easy']
 
-        if   options1 == " ":              lat_country  = 30.00;  lon_country  =  10.0; zoom_country = 0.45; markersize = 6; Screen_width =  550; Screen_height = 300
-        if   options1 == "london":         lat_country  = 51.52;  lon_country  = -0.1; zoom_country =  9.80; markersize = 5; Screen_width =  550; Screen_height = 300
+        if   options1 == " ":              lat_country  = 30.00;  lon_country  =  10.0; zoom_country = 0.6; markersize = 6; Screen_width =  600; Screen_height = 320
+        if   options1 == "london":         lat_country  = 51.52;  lon_country  = -0.1; zoom_country =  9.80; markersize = 5; Screen_width =  600; Screen_height = 320
 
 
         fig3 = go.Figure()
