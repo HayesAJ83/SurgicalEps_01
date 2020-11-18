d0e0e976ed305605073cbaac394cfdb5af74f262
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2020-11-18 21:49:55 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -668,31 +668,21 @@ def exp_geography():
         site_lon = time_df['Long_A1']           #df3['Long_A1']
         text = time_df['Eponym_easy'] + ', ' + time_df['CityOfEponym_A1'] + ', ' + time_df['Year'].astype(str)
         locations_name = time_df['Eponym_easy'] #df3['Eponym_easy']
+        figG1 = go.Figure()
 
 #        if portrait1 == "World":        lat_1 = 40.00;  lon_1 = 0.0; zoom_country = -0.45; markersize = 4; Screen_width =  350; Screen_height = 260;
 #        if portrait1 == " ":              lat_1 = 40.00;  lon_1 =   0.0; zoom_country = -0.45; markersize = 4; Screen_width =  350; Screen_height = 260
         if portrait1 == "All":
-            lat_1 = 40.00;  lon_1 =   0.0; zoom_country =  -0.45; markersize = 5; Screen_width =  350; Screen_height = 260
-            figG1 = go.Figure()
             figG1.add_trace(go.Scattermapbox(
-                lat=site_lat,lon=site_lon,
-                mode='markers',
-                marker=go.scattermapbox.Marker(
-                    size=markersize,color='yellow',
-                    opacity=0.7),
-                text=text,hoverinfo='text',
-                ))
-            figG1.update_layout(margin=dict(l=2, r=2, t=0, b=0))
-            figG1.update_layout(
-                width=Screen_width,height=Screen_height,
-                mapbox=dict(
-                    accesstoken=mapbox_access_token, bearing=0,
-                    center=dict(lat=lat_1,lon=lon_1),pitch=0,zoom=zoom_country,
+                lat=site_lat,lon=site_lon,mode='markers',
+                marker=go.scattermapbox.Marker(size=5,color='yellow',opacity=0.7),
+                text=text,hoverinfo='text'))
+            #figG1.update_layout(margin=dict(l=2, r=2, t=0, b=0))
+            figG1.update_layout(width=350,height=260,
+                mapbox=dict(accesstoken=mapbox_access_token, bearing=0,
+                    center=dict(lat=40.00,lon=0.0),pitch=0,zoom=-0.45,
                     style='satellite-streets'))
             st.write(figG1)
-
-
-
 
 #        if portrait1 == "Argentina":      lat_1 =-40.00;  lon_1 = -65.0; zoom_country =   2.5; markersize = 6; Screen_width =  350; Screen_height = 260
 #        if portrait1 == "Austria":        lat_1 = 47.20;  lon_1 =  13.4; zoom_country =  6.50; markersize = 8; Screen_width =  350; Screen_height = 260
@@ -701,20 +691,12 @@ def exp_geography():
 #        if portrait1 == "Chicago":        lat_1 = 42.00;  lon_1 = -88.0; zoom_country =   8.0; markersize = 9; Screen_width =  350; Screen_height = 260
 #        if portrait1 == "Denmark":        lat_1 = 56.00;  lon_1 =   9.8; zoom_country =  4.00; markersize = 8; Screen_width =  350; Screen_height = 260
         if portrait1 == "Edinburgh":
-            lat_1 = 55.94;  lon_1 =  -3.2; zoom_country =   9.0; markersize = 9; Screen_width =  350; Screen_height = 260
-            figG1 = go.Figure()
+            lat_1 = 55.94;  lon_1 =  -3.2; zoom_country =   9.0; markersize = 9
             figG1.add_trace(go.Scattermapbox(
-                lat=site_lat,lon=site_lon,
-                mode='markers',
-                marker=go.scattermapbox.Marker(
-                    size=markersize,color='yellow',
-                    opacity=0.7),
-                text=text,hoverinfo='text',
-                ))
-            figG1.update_layout(margin=dict(l=2, r=2, t=0, b=0))
-            figG1.update_layout(
-                width=Screen_width,height=Screen_height,
-                mapbox=dict(
+                lat=site_lat,lon=site_lon,mode='markers',
+                marker=go.scattermapbox.Marker(size=markersize,color='yellow',opacity=0.7),
+                text=text,hoverinfo='text'))
+            figG1.update_layout(width=350,height=260,mapbox=dict(
                     accesstoken=mapbox_access_token, bearing=0,
                     center=dict(lat=lat_1,lon=lon_1),pitch=0,zoom=zoom_country,
                     style='satellite-streets'))
@@ -731,7 +713,6 @@ def exp_geography():
 
         if portrait1 == "London":
             lat_1 = 51.52;  lon_1 =  -0.1; zoom_country =   8.5; markersize = 9; Screen_width =  350; Screen_height = 260;
-            figG1 = go.Figure()
             figG1.add_trace(go.Scattermapbox(
                 lat=site_lat,lon=site_lon,
                 mode='markers',
@@ -752,7 +733,6 @@ def exp_geography():
 #        if portrait1 == "Netherlands":    lat_1 = 52.00;  lon_1 =   5.0; zoom_country =   4.8; markersize = 8; Screen_width =  350; Screen_height = 260
         if portrait1 == "New York City":
             lat_1 = 40.80;  lon_1 = -73.9; zoom_country =   8.5; markersize = 9; Screen_width =  350; Screen_height = 260;
-            figG1 = go.Figure()
             figG1.add_trace(go.Scattermapbox(
                 lat=site_lat,lon=site_lon,
                 mode='markers',
@@ -776,7 +756,6 @@ def exp_geography():
 #        if portrait1 == "South America":  lat_1 =-28.00;  lon_1 = -65.0; zoom_country =   1.8; markersize = 6; Screen_width =  350; Screen_height = 260
         if portrait1 == "Sweden":
             lat_1 = 62.50;  lon_1 =  18.5; zoom_country =   3.0; markersize = 8; Screen_width =  350; Screen_height = 260;
-            figG1 = go.Figure()
             figG1.add_trace(go.Scattermapbox(
                 lat=site_lat,lon=site_lon,
                 mode='markers',
@@ -785,7 +764,6 @@ def exp_geography():
                     opacity=0.7),
                 text=text,hoverinfo='text',
                 ))
-            figG1.update_layout(margin=dict(l=2, r=2, t=0, b=0))
             figG1.update_layout(
                 width=Screen_width,height=Screen_height,
                 mapbox=dict(
