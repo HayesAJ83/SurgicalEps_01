4868976569bc56d87b6149a76e9108afb445b046
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2020-11-20 23:04:45 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -656,11 +656,11 @@ def exp_geography():
         if   options3 == "Germany":        lat_3 = 51.30; lon_3 =  10.2; zoom_country = 4.50; markersize =11; Screen_width =  700; Screen_height = 450
         if   options3 == "Hawaii":         lat_3 = 20.50; lon_3 =-157.3; zoom_country = 5.50; markersize = 9; Screen_width =  700; Screen_height = 450
         if   options3 == "Ireland":        lat_3 = 53.50; lon_3 =  -6.2; zoom_country = 5.00; markersize = 9; Screen_width =  700; Screen_height = 450
-        if   options3 == "Italy":          lat_3 = 41.50; lon_3 =  14.0; zoom_country = 4.00; markersize =10; Screen_width =  700; Screen_height = 450
+        if   options3 == "Italy":          lat_3 = 41.80; lon_3 =  14.0; zoom_country = 4.80; markersize =11; Screen_width =  700; Screen_height = 450
         if   options3 == "Japan":          lat_3 = 37.70; lon_3 = 135.5; zoom_country = 3.45; markersize = 9; Screen_width =  700; Screen_height = 450
         if   options3 == "London":         lat_3 = 51.54; lon_3 =  -0.1; zoom_country = 8.96; markersize =12; Screen_width =  700; Screen_height = 450
         if   options3 == "Netherlands":    lat_3 = 52.20; lon_3 =   5.0; zoom_country = 6.00; markersize =11; Screen_width =  700; Screen_height = 450
-        if   options3 == "New York City":  lat_3 = 40.78; lon_3 = -73.9; zoom_country = 8.40; markersize =11; Screen_width =  700; Screen_height = 450
+        if   options3 == "New York City":  lat_3 = 40.78; lon_3 = -73.9; zoom_country = 8.80; markersize =11; Screen_width =  700; Screen_height = 450
         if   options3 == "North America":  lat_3 = 51.00; lon_3 =  -103; zoom_country = 1.75; markersize = 9; Screen_width =  700; Screen_height = 450
         if   options3 == "Paris":          lat_3 = 48.86; lon_3 =  2.35; zoom_country = 10.2; markersize =12; Screen_width =  700; Screen_height = 450
         if   options3 == "Poland":         lat_3 = 52.50; lon_3 =  19.0; zoom_country =  5.0; markersize =12; Screen_width =  700; Screen_height = 450
