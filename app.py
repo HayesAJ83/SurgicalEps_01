52f4a8d27d1745fe3d2b299d6f4e720883c0e15a
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2020-12-20 21:13:11 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -656,9 +656,9 @@ def exp_journals():
         journal_spec = st.multiselect('Specialties:',options=list(U),
                            format_func=lambda x: ' ' if x == '1' else x,
                            default=[#'All Specialties',
-                                    'Anaesthetics','Bariatrics','Colorectal','Emergency Surgery','Endocrine','ENT',
+                                    'Anaesthetics','Bariatrics','Breast','Colorectal','Emergency Surgery','Endocrine','ENT',
                                     'General Surgery','Gynaecology','HPB','Hernia','Laparoscopic Surgery','Neurosurgery','Oesophagogastric',
-                                    'Orthopaedics','Paediatrics','Plastics','Transplant','Trauma','Urology',#'Vascular',
+                                    'Orthopaedics','Paediatrics','Plastics','Transplant','Trauma','Urology','Vascular',
                                     ])
         new_jrnls1 = df.loc[df['specialty'].str.contains('|'.join(journal_spec)) == True]
         new_jrnls2 = new_jrnls1.sort_values(by=['eponym'],ascending=True)
